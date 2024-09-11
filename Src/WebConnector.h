cc757623ce0854f5e610003900b4abd4b1686af8
PC-VICTOR\Victor <victormartinezsimon@gmail.com>
2024-09-11 20:02:12 +0200

    remove unused code

--- Src/WebConnector.h
+++ Src/WebConnector.h
@@ -15,8 +15,4 @@ public:
     std::string Get(const std::string& url) const;
 
     std::string Post(const std::string& url, const std::string& data) const;
-
-    void GetTelegramChatID(const std::string& telegramBotKey);
-
-    void WriteTelegramMessage(const std::string& telegramBotKey, const std::string& chatID, const std::string& msg);
 };
