1d98fb369edc5fe1f1f9e28085ba34f455759374
PC-VICTOR\Victor <victormartinezsimon@gmail.com>
2024-10-10 17:57:09 +0200

    add one example to parse a web that uses javascript

---
+++ scripts/scrape.py
@@ -0,0 +1,30 @@
+import sys
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+if len(sys.argv) < 3:
+    print("Use: python scrape.py <URL> <driverpath>")
+    sys.exit(1)
+
+url = sys.argv[1]
+path = sys.argv[2]
+
+service = webdriver.ChromeService(executable_path = path)
+
+# Headless mode, not working
+#chrome_options = Options()
+#chrome_options.add_argument("--headless")
+#driver = webdriver.Chrome(service=service, options=chrome_options)
+
+driver = webdriver.Chrome()
+
+driver.get(url)
+
+html = driver.page_source
+
+print(html)
+
+driver.quit()
+
